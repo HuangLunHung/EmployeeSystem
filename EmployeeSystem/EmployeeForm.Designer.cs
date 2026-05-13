435ee462330cdf045a1b8f12f5a95749f26e2d11
cbb109119 <cbb109119@nptu.edu.tw>
2026-05-13 22:43:26 +0800

    Finish put all UI element on the window

--- EmployeeSystem/EmployeeForm.Designer.cs
+++ EmployeeSystem/EmployeeForm.Designer.cs
@@ -29,6 +29,32 @@
         private void InitializeComponent()
         {
             lblTitle = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            panel2 = new Panel();
+            grpTool = new GroupBox();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            grpTool.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // lblTitle
@@ -40,14 +66,227 @@
             lblTitle.TabIndex = 0;
             lblTitle.Text = "員工管理系統 (Employee Management Syst)em";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 4);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 19);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 32);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 19);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 60);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 19);
+            label3.TabIndex = 3;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 88);
+            label4.Name = "label4";
+            label4.Size = new Size(51, 19);
+            label4.TabIndex = 4;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 116);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 19);
+            label5.TabIndex = 5;
+            label5.Text = "label5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(20, 14);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 19);
+            label6.TabIndex = 6;
+            label6.Text = "label6";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 7;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(6, 53);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 8;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(6, 81);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 9;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(6, 109);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 10;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(340, 11);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 11;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(240, 11);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 12;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label5);
+            panel1.Location = new Point(0, 52);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(334, 146);
+            panel1.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(109, 1);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(109, 29);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(109, 57);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(109, 85);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(109, 113);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(77, 11);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(157, 27);
+            textBox6.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.InactiveCaption;
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(button6);
+            panel2.Location = new Point(0, 204);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(445, 47);
+            panel2.TabIndex = 14;
+            // 
+            // grpTool
+            // 
+            grpTool.Controls.Add(button3);
+            grpTool.Controls.Add(button1);
+            grpTool.Controls.Add(button2);
+            grpTool.Controls.Add(button4);
+            grpTool.ForeColor = SystemColors.ControlText;
+            grpTool.Location = new Point(340, 53);
+            grpTool.Name = "grpTool";
+            grpTool.Size = new Size(105, 145);
+            grpTool.TabIndex = 15;
+            grpTool.TabStop = false;
+            grpTool.Text = "Tool";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(0, 250);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(984, 403);
+            dataGridView1.TabIndex = 16;
+            // 
             // EmployeeForm
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(982, 653);
+            Controls.Add(dataGridView1);
+            Controls.Add(grpTool);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Controls.Add(lblTitle);
             Name = "EmployeeForm";
             Text = "EmployeeForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            grpTool.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +294,27 @@
         #endregion
 
         private Label lblTitle;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Panel panel1;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox6;
+        private Panel panel2;
+        private GroupBox grpTool;
+        private DataGridView dataGridView1;
     }
 }
